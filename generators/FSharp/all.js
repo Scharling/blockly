229cfdac3ac432360487949494512e8344e6f540
Oliver Scharling Pedersen <olpe@itu.dk>
2022-03-30 13:14:53 +0200

    Made it so npm run build:compiled now also compiles FSharp_compressed.js

---
+++ generators/FSharp/all.js
@@ -0,0 +1,17 @@
+/**
+ * @fileoverview Complete helper functions for generating FSharp for
+ *     blocks.  This is the entrypoint for fsharp_compressed.js.
+ * @suppress {extraRequire}
+ */
+'use strict';
+
+goog.module('Blockly.FSharp.all');
+
+//goog.require('Blockly.FSharp.lists'); ?????
+goog.require('Blockly.FSharp.logic');
+//goog.require('Blockly.FSharp.math');
+//goog.require('Blockly.FSharp.procedures');
+//goog.require('Blockly.FSharp.texts');
+//goog.require('Blockly.FSharp.variables');
+//goog.require('Blockly.FSharp.variablesDynamic');
+
