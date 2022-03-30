5a0e6a2984bae7dff0b6e0f38f067d4319dea48f
Psiphph <sifamalie@gmail.com>
2022-03-30 12:11:03 +0200

    Translated most of logic to F#, ifelse missing

--- generators/F#/logic.js
+++ generators/F#/logic.js
@@ -1,82 +1,76 @@
 /**
- * @license
- * Copyright 2012 Google LLC
- * SPDX-License-Identifier: Apache-2.0
- */
-
-/**
- * @fileoverview Generating Python for logic blocks.
+ * @fileoverview Generating F# for logic blocks.
  */
 'use strict';
 
-goog.module('Blockly.Python.logic');
+goog.module('Blockly.F#.logic');
 
-const Python = goog.require('Blockly.Python');
+const FSharp = goog.require('Blockly.FSharp');
 
 
-Python['controls_if'] = function(block) {
-  // If/elseif/else condition.
-  let n = 0;
-  let code = '', branchCode, conditionCode;
-  if (Python.STATEMENT_PREFIX) {
-    // Automatic prefix insertion is switched off for this block.  Add manually.
-    code += Python.injectId(Python.STATEMENT_PREFIX, block);
-  }
-  do {
-    conditionCode =
-        Python.valueToCode(block, 'IF' + n, Python.ORDER_NONE) || 'False';
-    branchCode = Python.statementToCode(block, 'DO' + n) || Python.PASS;
-    if (Python.STATEMENT_SUFFIX) {
-      branchCode =
-          Python.prefixLines(
-              Python.injectId(Python.STATEMENT_SUFFIX, block), Python.INDENT) +
-          branchCode;
-    }
-    code += (n === 0 ? 'if ' : 'elif ') + conditionCode + ':\n' + branchCode;
-    n++;
-  } while (block.getInput('IF' + n));
+// FSharp['controls_if'] = function (block) {
+//   // If/elseif/else condition.
+//   let n = 0;
+//   let code = '', branchCode, conditionCode;
+//   if (FSharp.STATEMENT_PREFIX) {
+//     // Automatic prefix insertion is switched off for this block.  Add manually.
+//     code += FSharp.injectId(FSharp.STATEMENT_PREFIX, block);
+//   }
+//   do {
+//     conditionCode =
+//       FSharp.valueToCode(block, 'IF' + n, FSharp.ORDER_NONE) || 'false';
+//     branchCode = FSharp.statementToCode(block, 'DO' + n) || FSharp.PASS;
+//     if (FSharp.STATEMENT_SUFFIX) {
+//       branchCode =
+//         FSharp.prefixLines(
+//           FSharp.injectId(FSharp.STATEMENT_SUFFIX, block), FSharp.INDENT) +
+//         branchCode;
+//     }
+//     code += (n === 0 ? 'if ' : 'elif ') + conditionCode + ':\n' + branchCode;
+//     n++;
+//   } while (block.getInput('IF' + n));
 
-  if (block.getInput('ELSE') || Python.STATEMENT_SUFFIX) {
-    branchCode = Python.statementToCode(block, 'ELSE') || Python.PASS;
-    if (Python.STATEMENT_SUFFIX) {
-      branchCode =
-          Python.prefixLines(
-              Python.injectId(Python.STATEMENT_SUFFIX, block), Python.INDENT) +
-          branchCode;
-    }
-    code += 'else:\n' + branchCode;
-  }
-  return code;
-};
+//   if (block.getInput('ELSE') || FSharp.STATEMENT_SUFFIX) {
+//     branchCode = FSharp.statementToCode(block, 'ELSE') || FSharp.PASS;
+//     if (FSharp.STATEMENT_SUFFIX) {
+//       branchCode =
+//         FSharp.prefixLines(
+//           FSharp.injectId(FSharp.STATEMENT_SUFFIX, block), FSharp.INDENT) +
+//         branchCode;
+//     }
+//     code += 'else:\n' + branchCode;
+//   }
+//   return code;
+// };
 
-Python['controls_ifelse'] = Python['controls_if'];
+FSharp['controls_ifelse'] = FSharp['controls_if'];
 
-Python['logic_compare'] = function(block) {
+FSharp['logic_compare'] = function (block) {
   // Comparison operator.
   const OPERATORS =
-      {'EQ': '==', 'NEQ': '!=', 'LT': '<', 'LTE': '<=', 'GT': '>', 'GTE': '>='};
+    { 'EQ': '=', 'NEQ': '<>', 'LT': '<', 'LTE': '<=', 'GT': '>', 'GTE': '>=' };
   const operator = OPERATORS[block.getFieldValue('OP')];
-  const order = Python.ORDER_RELATIONAL;
-  const argument0 = Python.valueToCode(block, 'A', order) || '0';
-  const argument1 = Python.valueToCode(block, 'B', order) || '0';
+  const order = FSharp.ORDER_RELATIONAL;
+  const argument0 = FSharp.valueToCode(block, 'A', order) || '0';
+  const argument1 = FSharp.valueToCode(block, 'B', order) || '0';
   const code = argument0 + ' ' + operator + ' ' + argument1;
   return [code, order];
 };
 
-Python['logic_operation'] = function(block) {
+FSharp['logic_operation'] = function (block) {
   // Operations 'and', 'or'.
-  const operator = (block.getFieldValue('OP') === 'AND') ? 'and' : 'or';
+  const operator = (block.getFieldValue('OP') === 'AND') ? '&&' : '||';
   const order =
-      (operator === 'and') ? Python.ORDER_LOGICAL_AND : Python.ORDER_LOGICAL_OR;
-  let argument0 = Python.valueToCode(block, 'A', order);
-  let argument1 = Python.valueToCode(block, 'B', order);
+    (operator === '&&') ? FSharp.ORDER_LOGICAL_AND : FSharp.ORDER_LOGICAL_OR;
+  let argument0 = FSharp.valueToCode(block, 'A', order);
+  let argument1 = FSharp.valueToCode(block, 'B', order);
   if (!argument0 && !argument1) {
     // If there are no arguments, then the return value is false.
-    argument0 = 'False';
-    argument1 = 'False';
+    argument0 = 'false';
+    argument1 = 'false';
   } else {
     // Single missing arguments have no effect on the return value.
-    const defaultArgument = (operator === 'and') ? 'True' : 'False';
+    const defaultArgument = (operator === '&&') ? 'true' : 'false';
     if (!argument0) {
       argument0 = defaultArgument;
     }
@@ -88,33 +82,33 @@ Python['logic_operation'] = function(block) {
   return [code, order];
 };
 
-Python['logic_negate'] = function(block) {
+FSharp['logic_negate'] = function (block) {
   // Negation.
   const argument0 =
-      Python.valueToCode(block, 'BOOL', Python.ORDER_LOGICAL_NOT) || 'True';
+    FSharp.valueToCode(block, 'BOOL', Python.ORDER_LOGICAL_NOT) || 'true';
   const code = 'not ' + argument0;
-  return [code, Python.ORDER_LOGICAL_NOT];
+  return [code, FSharp.ORDER_LOGICAL_NOT];
 };
 
-Python['logic_boolean'] = function(block) {
+FSharp['logic_boolean'] = function (block) {
   // Boolean values true and false.
-  const code = (block.getFieldValue('BOOL') === 'TRUE') ? 'True' : 'False';
-  return [code, Python.ORDER_ATOMIC];
+  const code = (block.getFieldValue('BOOL') === 'TRUE') ? 'true' : 'false';
+  return [code, FSharp.ORDER_ATOMIC];
 };
 
-Python['logic_null'] = function(block) {
+FSharp['logic_null'] = function (block) {
   // Null data type.
-  return ['None', Python.ORDER_ATOMIC];
+  return ['()', FSharp.ORDER_ATOMIC];
 };
 
-Python['logic_ternary'] = function(block) {
+FSharp['logic_ternary'] = function (block) {
   // Ternary operator.
   const value_if =
-      Python.valueToCode(block, 'IF', Python.ORDER_CONDITIONAL) || 'False';
+    FSharp.valueToCode(block, 'IF', Python.ORDER_CONDITIONAL) || 'false';
   const value_then =
-      Python.valueToCode(block, 'THEN', Python.ORDER_CONDITIONAL) || 'None';
+    FSharp.valueToCode(block, 'THEN', Python.ORDER_CONDITIONAL) || 'None';
   const value_else =
-      Python.valueToCode(block, 'ELSE', Python.ORDER_CONDITIONAL) || 'None';
-  const code = value_then + ' if ' + value_if + ' else ' + value_else;
-  return [code, Python.ORDER_CONDITIONAL];
+    FSharp.valueToCode(block, 'ELSE', Python.ORDER_CONDITIONAL) || 'None';
+  const code = 'if ' + value_if + ' then ' + value_then + ' else ' + value_else;
+  return [code, FSharp.ORDER_CONDITIONAL];
 };
