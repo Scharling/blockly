230ad97772c619fb3f1f440354993969a50b751e
Psiphph <sifamalie@gmail.com>
2022-03-31 13:25:08 +0200

    Finished math translation to F# - not type checking for int/float mix

--- generators/FSharp/math.js
+++ generators/FSharp/math.js
@@ -48,86 +48,107 @@ FSharp['math_arithmetic'] = function (block) {
   const order = tuple[1];
   const argument0 = FSharp.valueToCode(block, 'A', order) || '0';
   const argument1 = FSharp.valueToCode(block, 'B', order) || '0';
+
+  // const arg0IsInt = FSharp.numberIsInt(argument0);
+  // const arg1IsInt = FSharp.numberIsInt(argument1);
+
+  // let code;
+  // console.log(FSharp.isNumber(argument0));
+  // console.log(FSharp.isNumber(argument1));
+  // console.log(arg0IsInt === arg1IsInt);
+  // if (FSharp.isNumber(argument0) && FSharp.isNumber(argument1)) {
+  //   if (!arg0IsInt && arg1IsInt) {
+  //     code = argument0 + operator + 'float ' + argument1;
+  //   } else if (arg0IsInt && !arg1IsInt) {
+  //     code = 'float ' + argument0 + operator + argument1;
+  //   } else {
+  //     code = argument0 + operator + argument1;
+  //   }
+  // } else {
+  //   code = 'float ' + argument0 + operator + 'float ' + argument1;
+  // }
+
   const code = argument0 + operator + argument1;
+
   return [code, order];
 };
 
-// FSharp['math_single'] = function(block) {
-//   // Math operators with single operand.
-//   const operator = block.getFieldValue('OP');
-//   let code;
-//   let arg;
-//   if (operator === 'NEG') {
-//     // Negation is a special case given its different operator precedence.
-//     code = FSharp.valueToCode(block, 'NUM', FSharp.ORDER_PREFIX_OPERATORS) || '0';
-//     return ['-' + code, FSharp.ORDER_PREFIX_OPERATORS];
-//   }
-//   FSharp.definitions_['import_math'] = 'import math';
-//   if (operator === 'SIN' || operator === 'COS' || operator === 'TAN') {
-//     arg = FSharp.valueToCode(block, 'NUM', FSharp.ORDER_MULTIPLICATIVE) || '0';
-//   } else {
-//     arg = FSharp.valueToCode(block, 'NUM', FSharp.ORDER_NONE) || '0';
-//   }
-//   // First, handle cases which generate values that don't need parentheses
-//   // wrapping the code.
-//   switch (operator) {
-//     case 'ABS':
-//       code = 'math.fabs(' + arg + ')';
-//       break;
-//     case 'ROOT':
-//       code = 'math.sqrt(' + arg + ')';
-//       break;
-//     case 'LN':
-//       code = 'math.log(' + arg + ')';
-//       break;
-//     case 'LOG10':
-//       code = 'math.log10(' + arg + ')';
-//       break;
-//     case 'EXP':
-//       code = 'math.exp(' + arg + ')';
-//       break;
-//     case 'POW10':
-//       code = 'math.pow(10,' + arg + ')';
-//       break;
-//     case 'ROUND':
-//       code = 'round(' + arg + ')';
-//       break;
-//     case 'ROUNDUP':
-//       code = 'math.ceil(' + arg + ')';
-//       break;
-//     case 'ROUNDDOWN':
-//       code = 'math.floor(' + arg + ')';
-//       break;
-//     case 'SIN':
-//       code = 'math.sin(' + arg + ' / 180.0 * math.pi)';
-//       break;
-//     case 'COS':
-//       code = 'math.cos(' + arg + ' / 180.0 * math.pi)';
-//       break;
-//     case 'TAN':
-//       code = 'math.tan(' + arg + ' / 180.0 * math.pi)';
-//       break;
-//   }
-//   if (code) {
-//     return [code, Python.ORDER_FUNCTION_CALL];
-//   }
-//   // Second, handle cases which generate values that may need parentheses
-//   // wrapping the code.
-//   switch (operator) {
-//     case 'ASIN':
-//       code = 'math.asin(' + arg + ') / math.pi * 180';
-//       break;
-//     case 'ACOS':
-//       code = 'math.acos(' + arg + ') / math.pi * 180';
-//       break;
-//     case 'ATAN':
-//       code = 'math.atan(' + arg + ') / math.pi * 180';
-//       break;
-//     default:
-//       throw Error('Unknown math operator: ' + operator);
-//   }
-//   return [code, Python.ORDER_MULTIPLICATIVE];
-// };
+FSharp['math_single'] = function (block) {
+  // Math operators with single operand.
+  const operator = block.getFieldValue('OP');
+  let code;
+  let arg;
+  if (operator === 'NEG') {
+    // Negation is a special case given its different operator precedence.
+    code = FSharp.valueToCode(block, 'NUM', FSharp.ORDER_PREFIX_OPERATORS) || '0';
+    return ['-' + code, FSharp.ORDER_PREFIX_OPERATORS];
+  }
+  FSharp.definitions_['open_system'] = 'open System';
+  if (operator === 'SIN' || operator === 'COS' || operator === 'TAN') {
+    arg = FSharp.valueToCode(block, 'NUM', FSharp.ORDER_MULTIPLICATIVE) || '0';
+  } else {
+    arg = FSharp.valueToCode(block, 'NUM', FSharp.ORDER_NONE) || '0';
+  }
+  // First, handle cases which generate values that don't need parentheses
+  // wrapping the code.
+  switch (operator) {
+    case 'ABS':
+      code = 'Math.Abs ' + arg;
+      break;
+    case 'ROOT':
+      code = 'Math.Sqrt ' + arg;
+      break;
+    case 'LN':
+      code = 'Math.Log ' + arg;
+      break;
+    case 'LOG10':
+      code = 'Math.Log10 ' + arg;
+      break;
+    case 'EXP':
+      code = 'Math.Exp ' + arg;
+      break;
+    case 'POW10':
+      code = 'Math.Pow(10,' + arg + ')';
+      break;
+    case 'ROUND':
+      code = 'Math.Round ' + arg;
+      break;
+    case 'ROUNDUP':
+      code = 'Math.Ceiling ' + arg;
+      break;
+    case 'ROUNDDOWN':
+      code = 'Math.Floor ' + arg;
+      break;
+    case 'SIN':
+      code = 'Math.Sin(' + arg + ' / 180.0 * Math.PI)';
+      break;
+    case 'COS':
+      code = 'Math.Cos(' + arg + ' / 180.0 * Math.PI)';
+      break;
+    case 'TAN':
+      code = 'Math.Tan(' + arg + ' / 180.0 * Math.PI)';
+      break;
+  }
+  if (code) {
+    return [code, FSharp.ORDER_FUNCTION_APPLICATION];
+  }
+  // Second, handle cases which generate values that may need parentheses
+  // wrapping the code.
+  switch (operator) {
+    case 'ASIN':
+      code = 'Math.Asin(' + arg + ') / math.pi * 180.0';
+      break;
+    case 'ACOS':
+      code = 'Math.Acos(' + arg + ') / math.pi * 180.0';
+      break;
+    case 'ATAN':
+      code = 'Math.Atan(' + arg + ') / math.pi * 180.0';
+      break;
+    default:
+      throw Error('Unknown math operator: ' + operator);
+  }
+  return [code, FSharp.ORDER_MULTIPLICATIVE];
+};
 
 FSharp['math_constant'] = function (block) {
   // Constants: PI, E, the Golden Ratio, sqrt(2), 1/sqrt(2), INFINITY.
@@ -146,67 +167,54 @@ FSharp['math_constant'] = function (block) {
   return CONSTANTS[constant];
 };
 
-// Python['math_number_property'] = function(block) {
-//   // Check if a number is even, odd, prime, whole, positive, or negative
-//   // or if it is divisible by certain number. Returns true or false.
-//   const number_to_check =
-//       Python.valueToCode(
-//           block, 'NUMBER_TO_CHECK', Python.ORDER_MULTIPLICATIVE) ||
-//       '0';
-//   const dropdown_property = block.getFieldValue('PROPERTY');
-//   let code;
-//   if (dropdown_property === 'PRIME') {
-//     Python.definitions_['import_math'] = 'import math';
-//     Python.definitions_['from_numbers_import_Number'] =
-//         'from numbers import Number';
-//     const functionName = Python.provideFunction_('math_isPrime', [
-//       'def ' + Python.FUNCTION_NAME_PLACEHOLDER_ + '(n):',
-//       '  # https://en.wikipedia.org/wiki/Primality_test#Naive_methods',
-//       '  # If n is not a number but a string, try parsing it.',
-//       '  if not isinstance(n, Number):', '    try:', '      n = float(n)',
-//       '    except:', '      return False',
-//       '  if n == 2 or n == 3:', '    return True',
-//       '  # False if n is negative, is 1, or not whole,' +
-//           ' or if n is divisible by 2 or 3.',
-//       '  if n <= 1 or n % 1 != 0 or n % 2 == 0 or n % 3 == 0:',
-//       '    return False',
-//       '  # Check all the numbers of form 6k +/- 1, up to sqrt(n).',
-//       '  for x in range(6, int(math.sqrt(n)) + 2, 6):',
-//       '    if n % (x - 1) == 0 or n % (x + 1) == 0:', '      return False',
-//       '  return True'
-//     ]);
-//     code = functionName + '(' + number_to_check + ')';
-//     return [code, Python.ORDER_FUNCTION_CALL];
-//   }
-//   switch (dropdown_property) {
-//     case 'EVEN':
-//       code = number_to_check + ' % 2 == 0';
-//       break;
-//     case 'ODD':
-//       code = number_to_check + ' % 2 == 1';
-//       break;
-//     case 'WHOLE':
-//       code = number_to_check + ' % 1 == 0';
-//       break;
-//     case 'POSITIVE':
-//       code = number_to_check + ' > 0';
-//       break;
-//     case 'NEGATIVE':
-//       code = number_to_check + ' < 0';
-//       break;
-//     case 'DIVISIBLE_BY': {
-//       const divisor =
-//           Python.valueToCode(block, 'DIVISOR', Python.ORDER_MULTIPLICATIVE);
-//       // If 'divisor' is some code that evals to 0, Python will raise an error.
-//       if (!divisor || divisor === '0') {
-//         return ['False', Python.ORDER_ATOMIC];
-//       }
-//       code = number_to_check + ' % ' + divisor + ' == 0';
-//       break;
-//     }
-//   }
-//   return [code, Python.ORDER_RELATIONAL];
-// };
+FSharp['math_number_property'] = function (block) {
+  // Check if a number is even, odd, prime, whole, positive, or negative
+  // or if it is divisible by certain number. Returns true or false.
+  const number_to_check =
+    FSharp.valueToCode(
+      block, 'NUMBER_TO_CHECK', FSharp.ORDER_MULTIPLICATIVE) ||
+    '0';
+  const dropdown_property = block.getFieldValue('PROPERTY');
+  let code;
+  if (dropdown_property === 'PRIME') {
+    FSharp.definitions_['open_system'] = 'open System';
+    const functionName = FSharp.provideFunction_('math_isPrime', [
+      'let ' + FSharp.FUNCTION_NAME_PLACEHOLDER_ + ' n =',
+      ' let sqrt = Math.Sqrt n |> int // square root of integer',
+      ' [ 2 .. sqrt ] |> List.forall (fun x -> n % x <> 0) // no divisors',
+    ]);
+    code = functionName + ' ' + number_to_check;
+    return [code, FSharp.ORDER_FUNCTION_CALL];
+  }
+  switch (dropdown_property) {
+    case 'EVEN':
+      code = number_to_check + ' % 2 = 0';
+      break;
+    case 'ODD':
+      code = number_to_check + ' % 2 = 1';
+      break;
+    case 'WHOLE':
+      code = number_to_check + ' % 1 = 0';
+      break;
+    case 'POSITIVE':
+      code = number_to_check + ' > 0';
+      break;
+    case 'NEGATIVE':
+      code = number_to_check + ' < 0';
+      break;
+    case 'DIVISIBLE_BY': {
+      const divisor =
+        FSharp.valueToCode(block, 'DIVISOR', FSharp.ORDER_MULTIPLICATIVE);
+      // If 'divisor' is some code that evals to 0, F# will raise an error.
+      if (!divisor || divisor === '0') {
+        return ['false', FSharp.ORDER_ATOMIC];
+      }
+      code = number_to_check + ' % ' + divisor + ' = 0';
+      break;
+    }
+  }
+  return [code, FSharp.ORDER_RELATIONAL];
+};
 
 // Rounding functions have a single operand.
 FSharp['math_round'] = FSharp['math_single'];
@@ -332,28 +340,28 @@ FSharp['math_constrain'] = function (block) {
   return [code, FSharp.ORDER_FUNCTION_APPLICATION];
 };
 
-// Python['math_random_int'] = function(block) {
-//   // Random integer between [X] and [Y].
-//   Python.definitions_['import_random'] = 'import random';
-//   const argument0 = Python.valueToCode(block, 'FROM', Python.ORDER_NONE) || '0';
-//   const argument1 = Python.valueToCode(block, 'TO', Python.ORDER_NONE) || '0';
-//   const code = 'random.randint(' + argument0 + ', ' + argument1 + ')';
-//   return [code, Python.ORDER_FUNCTION_CALL];
-// };
+FSharp['math_random_int'] = function (block) {
+  // Random integer between [X] and [Y].
+  FSharp.definitions_['open_system'] = 'open System';
+  const argument0 = FSharp.valueToCode(block, 'FROM', FSharp.ORDER_NONE) || '0';
+  const argument1 = FSharp.valueToCode(block, 'TO', FSharp.ORDER_NONE) || '0';
+  const code = 'Random().Next(' + argument0 + ', ' + argument1 + ' + 1)';
+  return [code, FSharp.ORDER_FUNCTION_APPLICATION];
+};
 
-// Python['math_random_float'] = function(block) {
-//   // Random fraction between 0 and 1.
-//   Python.definitions_['import_random'] = 'import random';
-//   return ['random.random()', Python.ORDER_FUNCTION_CALL];
-// };
+FSharp['math_random_float'] = function (block) {
+  // Random fraction between 0 and 1.
+  FSharp.definitions_['open_system'] = 'open System';
+  return ['Random().NextDouble()', FSharp.ORDER_FUNCTION_APPLICATION];
+};
 
-// Python['math_atan2'] = function(block) {
-//   // Arctangent of point (X, Y) in degrees from -180 to 180.
-//   Python.definitions_['import_math'] = 'import math';
-//   const argument0 = Python.valueToCode(block, 'X', Python.ORDER_NONE) || '0';
-//   const argument1 = Python.valueToCode(block, 'Y', Python.ORDER_NONE) || '0';
-//   return [
-//     'math.atan2(' + argument1 + ', ' + argument0 + ') / math.pi * 180',
-//     Python.ORDER_MULTIPLICATIVE
-//   ];
-// };
+FSharp['math_atan2'] = function (block) {
+  // Arctangent of point (X, Y) in degrees from -180 to 180.
+  FSharp.definitions_['open_system'] = 'open System';
+  const argument0 = FSharp.valueToCode(block, 'X', FSharp.ORDER_NONE) || '0';
+  const argument1 = FSharp.valueToCode(block, 'Y', FSharp.ORDER_NONE) || '0';
+  return [
+    'Math.Atan2(' + argument1 + ', ' + argument0 + ') / Math.PI * 180.0',
+    FSharp.ORDER_MULTIPLICATIVE
+  ];
+};
