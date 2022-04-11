50a286e7e5462823e7e6775d8df516c53ef98c94
Psiphph <sifamalie@gmail.com>
2022-04-11 15:03:57 +0200

    Translated type shelf

---
+++ generators/FSharp/types.js
@@ -0,0 +1,22 @@
+/**
+ * @fileoverview Generating F# for type blocks.
+ */
+'use strict';
+
+goog.module('Blockly.FSharp.types');
+const typeUtils = goog.require('Blockly.extra.utils.types')
+
+const FSharp = goog.require('Blockly.FSharp');
+
+FSharp['type_unit'] = function (block) {
+    const type = typeUtils.createTypeFromBlock(block);
+    return [type.getFSharpType(), FSharp.ORDER_ATOMIC];
+};
+
+FSharp['type_int'] = FSharp['type_unit'];
+FSharp['type_float'] = FSharp['type_unit'];
+FSharp['type_string'] = FSharp['type_unit'];
+FSharp['type_bool'] = FSharp['type_unit'];
+FSharp['type_unit'] = FSharp['type_unit'];
+FSharp['type_tuple'] = FSharp['type_unit'];
+FSharp['type_function'] = FSharp['type_unit'];
